8ced3198fba6f99966512c27914781c1515dc744
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-12 22:45:35 +0100

    add button to open form to create meals

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,28 @@
 import { Group } from "@mantine/core";
 import "./navbar.css";
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button } from '@mantine/core';
+import MealForm from "./CreateMealForm";
 
 export default function Navbar() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <div className="navbar">
+       <Modal opened={opened} onClose={close} title="Meal">
+       <MealForm/>
+
+      </Modal>
+
+    
       <Group>
         <img src="/meal-prep-icon.png" alt="meal-prep-icon" />
         <h3>Your Meals for the week</h3>
+        <Button variant="default" onClick={open}>
+        Create a meal for a day
+      </Button>
       </Group>
+    
     </div>
   );
 }
