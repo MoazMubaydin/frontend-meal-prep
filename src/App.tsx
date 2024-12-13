987c8c1203f5c9ed56e64eefbcff4566679bdbc6
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-13 13:08:57 +0100

    implement routes to recipe details

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import RecipeCard from "./Pages/RecipeCard";
+import RecipeDetails from "./Components/RecipeDetails";
 function App() {
   return (
     <>
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<RecipeCard />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+
         <Route path="/about" />
       </Routes>
     </>
