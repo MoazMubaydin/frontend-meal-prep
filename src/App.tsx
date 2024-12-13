9bfa6f59ff92c44cf02939893fa116814b70c21e
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-13 07:31:10 +0100

    add route to recipes cards page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import RecipeCard from "./Pages/RecipeCard";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" />
+        <Route path="/recipes" element={<RecipeCard />} />
         <Route path="/about" />
       </Routes>
     </>
