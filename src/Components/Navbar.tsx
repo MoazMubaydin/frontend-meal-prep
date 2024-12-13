7503caea2b3dd7a7142dbf144379b4f536e7c149
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-13 13:07:42 +0100

    add pop up modal for creating a recipe + chaning to UseState

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,28 +1,47 @@
 import { Group } from "@mantine/core";
 import "./navbar.css";
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
+import { Modal, Button } from "@mantine/core";
 import MealForm from "./CreateMealForm";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import CreateRecipe from "./CreateRecipe";
 
 export default function Navbar() {
-  const [opened, { open, close }] = useDisclosure(false);
+  const [isMealModalOpen, setMealModalOpen] = useState(false);
+  const [isRecipeModalOpen, setRecipeModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <div className="navbar">
-       <Modal opened={opened} onClose={close} title="Meal">
-       <MealForm/>
-
+      <Modal
+        opened={isMealModalOpen}
+        onClose={() => setMealModalOpen(false)}
+        title="Create a Meal"
+      >
+        <MealForm />
       </Modal>
-
-    
+      <Modal
+        opened={isRecipeModalOpen}
+        onClose={() => setRecipeModalOpen(false)}
+        title="Create a Recipe"
+      >
+        <CreateRecipe />
+      </Modal>
       <Group>
-        <img src="/meal-prep-icon.png" alt="meal-prep-icon" />
+        <img
+          onClick={() => navigate("/")}
+          src="/meal-prep-icon.png"
+          alt="meal-prep-icon"
+        />
         <h3>Your Meals for the week</h3>
-        <Button variant="default" onClick={open}>
-        Create a meal for a day
-      </Button>
+        <Button variant="default" onClick={() => setMealModalOpen(true)}>
+          Create a meal
+        </Button>
+        <Button variant="default" onClick={() => setRecipeModalOpen(true)}>
+          Create a Recipe
+        </Button>
+        <Button onClick={() => navigate(`/recipes`)}>View Recipes</Button>
       </Group>
-    
     </div>
   );
 }
